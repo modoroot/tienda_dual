5e0b099f0f0bdbacbb768cb1a9ed8005473af4b7
amna <antoniomiguel2001@hotmail.es>
2022-11-11 12:45:39 +0100

    fino select/insert

---
+++ tienda_dual/src/conn/conn.php
@@ -0,0 +1,8 @@
+<?php
+$BD_USUARIO = 'localhost';
+$BD_BASE = 'tienda_dual';
+$BD_USER = 'root';
+$BD_PASS = 'root';
+// CADENA DE CONEXIÓN
+$pdo = new PDO('mysql:host=' . $BD_USUARIO . ';dbname=' . $BD_BASE, $BD_USER, $BD_PASS);
+$pdo->exec("set names utf8mb4");
---
+++ tienda_dual/src/usuario.php
@@ -0,0 +1,36 @@
+<?php
+
+function getUsuarios(){
+    include 'conn/conn.php';
+    //consulta preparada: se compila antes de ejecutar
+    $stmt = $pdo->prepare('SELECT * FROM usuario');
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+//binding de las columnas de la tabla variables con sus tipos de datos
+//correspondientes
+    while($row = $stmt->fetch()){
+        echo "ID: {$row["id_usuario"]} ";
+        echo "\n";
+        echo "Nombre: {$row["nombre"]} ";
+        echo "\n";
+        echo "Perfil: {$row["nombre_perfil"]} ";
+        echo "\n";
+        echo "---------\n";
+    }
+}
+getUsuarios();
+function setUsuario($nombre,$username, $password, $nombre_perfil,$id_privilegio){
+    include 'conn/conn.php';
+    // Prepare
+    $stmt = $pdo->prepare("INSERT INTO usuario (id_usuario,nombre,username,password,nombre_perfil,id_privilegio)
+VALUES (NULL, ?, ?, ?, ?, ?)");
+// Bind
+    $stmt->bindParam(1, $nombre);
+    $stmt->bindParam(2, $username);
+    $stmt->bindParam(3, $password);
+    $stmt->bindParam(4, $nombre_perfil);
+    $stmt->bindParam(5, $id_privilegio);
+// Excecute
+    $stmt->execute();
+}
+setUsuario("test insert nombre","test insert username","test insert pwd","usuario",1);
