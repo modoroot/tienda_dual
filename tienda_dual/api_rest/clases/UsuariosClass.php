add516587ed2b5fba661511da37e24ea04a7734c
amna <antoniomiguel2001@hotmail.es>
2023-03-08 13:54:37 +0100

    método PUT de API joya

--- tienda_dual/api_rest/clases/UsuariosClass.php
+++ tienda_dual/api_rest/clases/UsuariosClass.php
@@ -49,13 +49,13 @@ class UsuariosClass extends Conexion
             $this->id_privilegio = $datos['id_privilegio'];
 
             $verificar = $this->insertarUsuario();
-            if($verificar){
+            if ($verificar) {
                 $respuesta = $_respuesta->response;
                 $respuesta["result"] = array(
                     "id_usuario" => $verificar
                 );
                 return $respuesta;
-            }else{
+            } else {
                 return $_respuesta->error_500();
             }
         }
@@ -68,6 +68,56 @@ class UsuariosClass extends Conexion
             . $this->email . "', '" . $this->id_privilegio . "')";
         $verificar = parent::nonQueryId($query);
         if ($verificar) {
+            return $verificar;
+        } else {
+            return 0;
+        }
+    }
+
+    public function put($json)
+    {
+        $_respuesta = new Respuesta();
+        $datos = json_decode($json, true);
+        if (!isset($datos['id_usuario'])) {
+            return $_respuesta->error_400();
+        } else {
+            $this->id_usuario = $datos['id_usuario'];
+            //Verificar si se ha introducido un nombre
+            if (isset($datos['nombre'])) {
+                $this->nombre = $datos['nombre'];
+            }
+            if (isset($datos['username'])) {
+                $this->username = $datos['username'];
+            }
+            if (isset($datos['password'])) {
+                $this->password = $datos['password'];
+            }
+            //Verificar si se ha introducido un email
+            if (isset($datos['email'])) {
+                $this->email = $datos['email'];
+            }
+            if (isset($datos['id_privilegio'])) {
+                $this->id_privilegio = $datos['id_privilegio'];
+            }
+            $verificar = $this->actualizarUsuario();
+            if ($verificar) {
+                $respuesta = $_respuesta->response;
+                $respuesta["result"] = array(
+                    "id_usuario" => $this->id_usuario
+                );
+                return $respuesta;
+            } else {
+                return $_respuesta->error_500();
+            }
+        }
+    }
+    private function actualizarUsuario()
+    {
+        $query = "UPDATE " . $this->tabla . " SET nombre = '" . $this->nombre . "', username = '"
+         . $this->username . "', password = '" . $this->password . "', email = '" . $this->email 
+         . "', id_privilegio = '" . $this->id_privilegio . "' WHERE id_usuario = '" . $this->id_usuario . "'";
+        $verificar = parent::nonQuery($query);
+        if ($verificar >= 1) {
             return $verificar;
         } else {
             return 0;
--- tienda_dual/api_rest/usuarios.php
+++ tienda_dual/api_rest/usuarios.php
@@ -37,7 +37,19 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
     }
     echo json_encode($datosArray);
 } else if ($_SERVER['REQUEST_METHOD'] == "PUT") {
-    echo "PUT";
+    //Recepción de datos enviados por el cliente
+    $postBody = file_get_contents("php://input");
+    //Se envía al manejador
+    $datosArray = $_usuarios->put($postBody);
+    //Se envía respuesta al cliente
+    header("Content-Type: application/json");
+    if (isset($datosArray["result"]["error_id"])) {
+        $responseCode = $datosArray["result"]["error_id"];
+        http_response_code($responseCode);
+    } else {
+        http_response_code(200);
+    }
+    echo json_encode($datosArray);
 } else if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
     echo "DELETE";
 } else {
